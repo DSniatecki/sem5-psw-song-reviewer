abee04938d03fd6d50bfae350e2652c68778dee2
Damian Śniatecki <damian.sniatecki@axisdata.net>
2020-12-01 23:59:33 +0100

    Extend main website

--- song-reviewer-alerts.js
+++ song-reviewer-alerts.js
@@ -1,23 +1,10 @@
-function hideAlert() {
-    document.getElementById("alert").innerHTML = ''
+function loadWebsite() {
+    setTimeout(() => {
+        document.getElementById("formAlert").innerHTML = ''
+    }, 5000);
+
+    document.getElementById("formAlertCloseBtn").addEventListener("click", (e) => {
+        document.getElementById("formAlert").innerHTML = ''
+    })
 }
 
-function showAlert(text, color) {
-    document.getElementById("alert").innerHTML = `
-    <div class="alert" style="background-color: ${color};">
-        <span class="closebtn" onclick="hideAlert()">&times;</span>
-        ${text} 
-    </div>
-    `
-}
-
-function submitReview() {
-    showAlert("Review was added!", "#02cb04")
-    setTimeout(hideAlert, 5000);
-}
-
-
-function resetReview() {
-    showAlert("Review data was cleared!", "#f44336")
-    setTimeout(hideAlert, 5000);
-}
--- song-reviewer.php
+++ song-reviewer.php
@@ -1,5 +1,24 @@
 <?php
 include 'data.php';
+
+function createSongReview(array $rawSongReview)
+{
+    return array(
+        "song" => array(
+            "name" => $rawSongReview['songName'],
+            "artist" => $rawSongReview['artistName'],
+            "album" => $rawSongReview['albumName'],
+        ),
+        "reviewer" => array(
+            "nickname" => $rawSongReview['reviewerNickname'],
+            "email" => $rawSongReview['reviewerEmail'],
+            "isFemale" => $rawSongReview['isFemale']
+        ),
+        "review" => $rawSongReview['review']
+    );
+}
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html">
@@ -9,49 +28,57 @@ include 'data.php';
     <meta name="keywords" content="Log, Logging">
     <meta name="authors" content="Damian Śniatecki, Daniel Eryk Wojciechowski">
     <link rel="stylesheet" type="text/css" href="song-reviewer.css">
-    <script type="text/javascript" src="song-reviewer.js"></script>
     <script type="text/javascript" src="song-reviewer-alerts.js"></script>
 </head>
-<body>
+<body onload="loadWebsite()">
 <header>Song Reviewer</header>
-<div id="alert"></div>
 <?php
 if (isset($_POST['submit'])) {
-    $newSongReview = array(
-        "song" => array(
-            "name" => $_POST['songName'],
-            "artist" => $_POST['artistName'],
-            "youtubeUrl" => $_POST['songYoutubeUrl'],
-            "album" => $_POST['albumName'],
-        ),
-        "reviewer" => array(
-            "nickname" => $_POST['reviewerNickname'],
-            "email" => $_POST['reviewerEmail']
-        ),
-        "review" => $_POST['review']
-    );
-    array_push($songReviews, $newSongReview);
+    $name = $_POST["reviewerEmail"];
+    if (!preg_match(EMAIL_REGEXP, $name)) { ?>
+        <div id="formAlert">
+            <div class="alert" style="background-color: red;">
+                <span id="formAlertCloseBtn" class="closebtn">&times;</span>
+                <?php echo "Wrong email!" ?>
+            </div>
+        </div>
+        <?php
+    } else {
+        ?>
+        <div id="formAlert">
+            <div class="alert" style="background-color: #0e9b0e;">
+                <span id="formAlertCloseBtn" class="closebtn">&times;</span>
+                Review was added !
+            </div>
+        </div>
+        <?php
+        array_push($songReviews, createSongReview($_POST));
+    }
 }
 ?>
 <section>
     <h3><?php echo FORM_TITLE ?></h3>
     <form id="addNewReviewForm" name="newReviewForm" method="post" autocomplete="on">
         <p>
+            <label>Artist:
+                <select name="artistName" autocomplete="on">
+                    <?php foreach ($artists as $artist) { ?>
+                        <option value="<?php echo $artist ?>">
+                            <?php echo $artist ?>
+                        </option>
+                    <?php } ?>
+                </select>
+            </label>
+        </p>
+        <p>
             <label>Song name <input name="songName" type="text" minlength="2" maxlength="40" placeholder="Song name"
                                     required/></label>
-        <p>
-        <p>
-            <label>Artist <input name="artistName" type="text" minlength="1" maxlength="40" placeholder="Artist name"
-                                 required/></label>
-        <p>
-        <p>
-            <label>Youtube link <input name="songYoutubeUrl" type="url" minlength="5" maxlength="500"
-                                       placeholder="http://songcover.com" required/></label>
-        <p>
+        </p>
         <p>
             <label>Album <input name="albumName" type="text" minlength="2" maxlength="40"
                                 placeholder="Album name"/></label>
-        <p>
+        </p>
+
         <p>
             <label>Review <textarea name="review" rows="10" cols="30" placeholder="It blew me away !!"
                                     required></textarea></label>
@@ -59,13 +86,22 @@ if (isset($_POST['submit'])) {
         <p>
         <p><label>Your nickname <input name="reviewerNickname" type="text" minlength="2" maxlength="40"
                                        placeholder="Your nickname" required/></label>
+        </p>
         <p>
-            <label>Your email <input name="reviewerEmail" type="email" placeholder="reviewer@domain.com"
-                                     required></label></p>
+            <label>Your email <input name="reviewerEmail" type="text" placeholder="reviewer@domain.com"
+                                     required></label>
+        </p>
+        <p> Gender:
+            <label for="louie"> Female <input type="radio" name="isFemale" value="true"></label>
+            <label for="louie"> Male <input type="radio" name="isFemale" value="false"></label>
+        </p>
+
         <p>
             <input type="submit" name="submit" value="Create review"/>
             <input type="reset" onclick="resetReview()" value="Clear"/>
         </p>
+
+
     </form>
 </section>
 <?php foreach ($songReviews as $songReview) { ?>
@@ -74,7 +110,9 @@ if (isset($_POST['submit'])) {
         <p><strong>Artist: </strong><?php echo $songReview["song"]["artist"] ?></p>
         <p><strong>Album: </strong><?php echo $songReview["song"]["album"] ?></p>
         <p><strong>Review: </strong><?php echo $songReview["review"] ?></p>
-        <p><strong>Nickname: </strong><?php echo $songReview["reviewer"]["nickname"] ?></p>
+        <p><strong>Reviewer: </strong><?php
+            echo $songReview["reviewer"]["isFemale"] ? "Ms. " : "Mr. ";
+            echo $songReview["reviewer"]["nickname"] ?></p>
         <p><strong>Reviewer email: </strong><?php echo $songReview["reviewer"]["email"] ?></p>
     </section>
 <?php } ?>
