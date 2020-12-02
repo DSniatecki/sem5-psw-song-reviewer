281c115f12e7635f68ef20bacb487d6871aa4080
Jan Onua <janonua@gmail.com>
2020-12-02 06:38:49 +0100

    Added concert-reviewer (not finished yet)

---
+++ concert-reviewer.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Concert reviewer</title>
+    <meta charset="UTF-8">
+    <meta name="keywords" content="Concert, Reviews">
+    <meta name="authors" content="Damian Śniatecki, Jan Wantuła">
+    <link rel="stylesheet" type="text/css" href="song-reviewer.css">
+    <script type="text/javascript" src="song-reviewer-alerts.js"></script>
+</head>
+
+<body>
+<header>Concert reviewer</header>
+  <section>
+    <form id ="addConcertReviewForm" name ="newConcertForm" method="get" action="sent.php">
+      <p>
+        <label>Music type:
+          <select name = "musicType">
+            <?php
+            include 'data.php';
+
+            for ($i = 0; $i < count($song_types) ; $i++){
+                  print("<option value = \"$song_types[$i]\">");
+                  print("$song_types[$i]");
+                  print("</option>");
+             } ?>
+          </select>
+        </label>
+      </p>
+
+      <p>
+        <label>Artist:
+          <input name="artistName" type="text" minlength="2"
+          maxlength="50" placeholder="Input artist" required/>
+        </label>
+      </p>
+      <p>
+          <label>Your review:<textarea name="review" rows="10" cols="30"
+            placeholder="It was splendid!!!" required></textarea>
+          </label>
+      </p>
+
+      <p>
+        <label>When did the concert take place?:
+          <input name="concertDate" type="date" required/>
+        </label>
+      </p>
+
+      <p>
+        <label>Your nickname:
+          <input name="reviewerNickname" type="text" minlength="2" maxlength="40"
+                          placeholder="Your nickname" required/>
+        </label>
+      </p>
+
+      <p>
+          <input type="submit" name="submit" value="Create review"/>
+          <input type="reset" value="Clear"/>
+      </p>
+    </form>
+  </section>
+</body>
+</html>
---
+++ sent.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Submit</title>
+    <meta charset="UTF-8">
+    <meta name="keywords" content="Thanks, Review, Concert">
+    <meta name="authors" content="Damian Śniatecki, Jan Wantuła">
+    <link rel="stylesheet" type="text/css" href="song-reviewer.css">
+</head>
+
+<body>
+<header>Thanks for your review!</header>
+  <section>
+      <p>
+
+      </p>
+
+  </section>
+</body>
+</html>
